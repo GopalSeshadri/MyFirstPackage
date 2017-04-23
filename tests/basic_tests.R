802767b521ae1eff6fee09415fcf29fa8697d819
Gopal Seshadri <gopalavarmanseshadri@gmail.com>
2017-04-23 13:24:32 +0530

    First Commit

---
+++ tests/basic_tests.R
@@ -0,0 +1,9 @@
+test_that("basic_tests", {
+  year_array <- c("2000", "2016", "2036", "2050", "2090")
+  for(i in year_array)
+  {
+    file_name <- MyFirstPackage::make_filename(i)
+    expect_that(file_name, equals(paste0("accident_",i,".csv.bz2")))
+  }
+})
+
